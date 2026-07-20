57ba1257b796228449577a44215a0d7c65842f1b
Julia Leite Noronha <julia.noronha@qaracter.com>
2026-07-20 08:27:31 -0300

    ex03 da aula 13

--- src/aula13/ex03/soma.java
+++ src/aula13/ex03/soma.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class soma {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        int soma = num1 + num2;
+        System.out.printf("A soma é: %d\n", soma);
     }
 }
