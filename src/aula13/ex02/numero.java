0c43098abd9c8af4aa7e56724a25932c7d660425
Julia Leite Noronha <julia.noronha@qaracter.com>
2026-07-20 08:21:37 -0300

    ex02 - aula 13

---
+++ src/aula13/ex02/numero.java
@@ -0,0 +1,13 @@
+package aula13.ex02;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class numero {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual sua idade?");
+        int idade = scanner.nextInt();
+        System.out.println("O número informado foi " + idade);
+    }
+}
